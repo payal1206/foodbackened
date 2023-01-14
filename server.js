21723d40c1680cfed475567a42fdc66619de4bf4
payal1206 <68314999+payal1206@users.noreply.github.com>
2023-01-14 14:41:08 +0530

    added .env file

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
+require('dotenv').config();
 const app = express();
-const port = 3000;
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}!`);
+app.listen(process.env.PORT, () => {
+  console.log(`app listening on port ${process.env.PORT}!`);
 });
