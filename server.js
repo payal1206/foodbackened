ed0626d98b81893d4edbc99781846db8936b6efe
payal1206 <68314999+payal1206@users.noreply.github.com>
2023-02-14 23:18:58 +0530

    api's request

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 app.get("/", (req, res) => {
-  res.send(
-    "Hello Worldppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp!"
-  );
+  res.send("Hello World!");
+});
+
+app.get("/addRestaurant", (req, res) => {
+  res.json({ status: 200, message: "added" });
 });
 
 app.listen(process.env.PORT, () => {
